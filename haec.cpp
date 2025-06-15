770d2f1697f25dad651e95a6df0e31a3d1a1667f
Constantijn <andriuvipip@protonmail.com>
2025-06-14 23:19:52 -0400

    aegyptus

---
+++ haec.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	cout<<"Dame un numeraso"<<endl;
+	int numero;
+	cin>>numero;
+	while (numero >0){
+		cout<<"sigue pasando numerasos"<<endl;
+		cin>>numero;
+
+	}
+cout<<"tengan cuidado es el"<<endl;
+
+return 0;
+}
