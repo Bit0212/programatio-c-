ec9ecdb479ba8e0af2ff03e0ac43af63db83ce00
Andres <Andriuvipip@protonmail.com>
2025-07-06 18:41:45 -0400

    new programs added

---
+++ contadordepares.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+	cout<<"Dame un numerito"<<endl;
+	int numerito;
+	cin>>numerito;
+	int b;
+	int c;
+	for(b = 0, c=0;b<numerito;b+=2,c++){
+		cout<<b<<endl;
+		cout<<"Estos son los numeros pares que le siguen a tu numero detras: "<<c<<endl;
+		
+		
+	}
+		
+	
+	return 0;
+}
