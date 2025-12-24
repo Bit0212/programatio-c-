0f9bafff65bb238a592a29d0d667b196f9f3d8a1
Andres <Andriuvipip@protonmail.com>
2025-12-23 22:18:26 -0300

    added fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main(){
+  std::cout<<"hola crack pasame algo jeje "<<std::endl;
+  int x= 0,u=0, y=1,z=1;
+  std::cin>>u;
+  std::cout<<" 1 ";
+  for(int i =1; i<u; i++){
+z = x + y;
+std::cout<<z<<"  ";
+x = y;
+y = z;
+
+
+  }
+
+  return 0;
+}
