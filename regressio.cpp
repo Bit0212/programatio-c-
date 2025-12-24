2f642787461ff061d3f2a0ce4edbaf2de9ba308e
Andres <Andriuvipip@protonmail.com>
2025-12-23 21:45:46 -0300

    added regressio.cpp

---
+++ regressio.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int regresionen(int a){
+  int b =0;
+for(int i =0; i<=a; i++){
+int b = i+1;
+
+}
+return b;
+}
+int main(){
+  int a,e  =0;
+  std::cout<<"hola jeje me gustaria que me dieras un numerillo jeje"<<std::endl;
+  std::cin>>a;
+for(int i = 0; i<=a; i++){
+  e = regresionen(a)-i;
+
+}
+std::cout<<e;
+  return 0;
+}
