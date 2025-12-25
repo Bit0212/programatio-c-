18f70414d0a2ed2b47729c3550c22bf530657fe6
Andres <Andriuvipip@protonmail.com>
2025-12-25 19:01:22 -0300

    added primus.cpp

---
+++ primus.cpp
@@ -0,0 +1,25 @@
+#include <climits>
+#include <iostream>
+int main() {
+int a =0;
+    std::cout<<"pasame un numero crack"<<std::endl;
+    std::cin>>a;
+    if(a>2){
+        std::cout<<"es imposible descomponerlo en factores primos crack un saludo "<<std::endl;
+
+    }
+    for(int i =2; i*i<=a; i++){
+        while(a% i ==0){
+            std::cout<<i<<" ";
+            a/=i;
+            if(a>1){
+                std::cout<<"rouge "<<std::endl;
+            }
+        }
+
+    }
+if(a>1){
+    std::cout<<a<<std::endl;
+}
+    return 0;
+}
