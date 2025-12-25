d18b7d16eff7330f6ca1fefb78fbc1f5408b934d
Andres <Andriuvipip@protonmail.com>
2025-12-25 20:45:38 -0300

    added elemult.cpp

---
+++ elemult.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+   int ongrie[]={1,2,4,5,5,22,2,5,6,78,8};
+    int multam =1;
+    for(int i =0; i<10; i++){
+multam*=ongrie[i];
+
+    }
+    std::cout<<"hola te doy este valor porque me salio de la nada jeje "<<multam<<std::endl;
+    return 0;
+}
