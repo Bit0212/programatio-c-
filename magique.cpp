0fb2059e86dbc56ca2767fa7c349d13984e93313
Andres <Andriuvipip@protonmail.com>
2025-12-25 17:37:11 -0300

    modified magique.cpp

--- magique.cpp
+++ magique.cpp
@@ -1,26 +1,24 @@
-#include <iostream>
-#include <ctime>
 #include <cstdlib>
-int main(){
-int numeris, datui, conteur = 0;
-std::srand(time(NULL));
-  datui = 1+std::rand()%(100);
-while(numeris!=datui){
+#include <ctime>
+#include <iostream>
+int main() {
+  int numeris, datui, conteur = 0;
+  std::srand(time(NULL));
+  datui = 1 + std::rand() % (100);
+  while (numeris != datui) {
 
-  std::cout<<"hola crack pasame un dato jeje\n";std::cin>>numeris;
-  if(numeris>datui){
-    std::cout<<"\n pasame un dato menor che \n";
+    std::cout << "hola crack pasame un dato jeje\n";
+    std::cin >> numeris;
+    if (numeris > datui) {
+      std::cout << "\n pasame un dato menor che \n";
 
+    } else {
+      std::cout << "\n Pasame un dato mayor che \n";
+    }
+    conteur++;
   }
-  else{
-    std::cout<<"\n Pasame un dato mayor che \n";
-
-  }
-  conteur++;
-}
-std::cout<<"\n le atinaste crack un saludo n\ "<<"te llevo esta cantidad de intentos"<<conteur;
-
-
+  std::cout << "\n le atinaste crack un saludo \n "
+            << "te llevo esta cantidad de intentos" << conteur;
 
   return 0;
 }
