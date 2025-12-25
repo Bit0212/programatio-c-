f5e15222941c78ce5c3fd2bc6f8a56f2c027fad0
Andres <Andriuvipip@protonmail.com>
2025-12-25 20:58:27 -0300

    addded way.cpp

---
+++ way.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main(){
+int de;
+int maineis[de];
+std::cout<<"hola pasame un numerillo jeje je je "<<std::endl;
+std::cin>>de;
+for(int i =0; i<de; i++){
+    std::cout<<"pasame los dichosos datos :";std::cin>>maineis[i];std::cout<<"\n";
+}
+for(int i = 0; i<de; i++){
+
+    std::cout<<maineis[i]<<"\n";
+}
+    return 0;
+}
