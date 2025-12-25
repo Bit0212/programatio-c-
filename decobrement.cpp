5ebf11dac44a1822644a833d61c65ec57e570bc2
Andres <Andriuvipip@protonmail.com>
2025-12-25 20:36:20 -0300

    added decobrement.cpp

---
+++ decobrement.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+int deutschland [5] = {1,2,3,4,5};
+int sum = 0; 
+for(int i = 0; i<5; i++){
+
+    sum+=deutschland[i];
+}
+std::cout<<"elepepe :"<<sum<<std::endl;
+
+    return 0;
+}
