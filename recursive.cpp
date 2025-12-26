2d1c02e74492373e53a7f468f9e301f914b56b46
Andres <Andriuvipip@protonmail.com>
2025-12-25 21:02:19 -0300

    added recursive.cpp

---
+++ recursive.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+int maris[]= {1,2,34,45,2,4,6};
+for(int i = 7; i>0; i--){
+
+    std::cout<<"bueno aqui esta el numero :"<<maris[i]<<std::endl;
+}
+    return 0;
+}
